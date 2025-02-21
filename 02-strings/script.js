a43c2421efa5f8abcadbdedce1cb344fc0334a58
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-02-21 13:39:02 +0100

    feat: complete 2 task strings

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -11,6 +11,14 @@ console.log(name);
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
 
+let number = 8;
+let text = 'text';
+let bool = true;
+console.log(number);
+console.log(text);
+console.log(bool);
+
+
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
